59a754f5838249a27d3b364ea9050a0dc48989e3
Programming Bees <hasanakash799@gmail.com>
2022-06-26 00:25:17 +0600

    This app is build by jahid hasan. Here i am using mainly React for Frontend and Django for Backend. This project is build for learning purpose.

---
+++ main/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += [
+        re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    ]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    ]
